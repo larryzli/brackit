7e32ff9d9aae6e91ef5273ddc9b3c3b6eeb5ccd2
Larry Li <larry.zy.li@gmail.com>
2018-05-10 16:43:42 -0500

    add auth to router

--- server/masterRouter.js
+++ server/masterRouter.js
@@ -1,8 +1,10 @@
 const userRoutes = require(`${__dirname}/controllers/user/userRoutes`);
 const matchRoutes = require(`${__dirname}/controllers/match/matchRoutes`);
 const bracketRoutes = require(`${__dirname}/controllers/bracket/bracketRoutes`);
+const authRoutes = require(`${__dirname}/controllers/auth/authRoutes`);
 
 module.exports = function(app) {
+  authRoutes(app);
   userRoutes(app);
   matchRoutes(app);
   bracketRoutes(app);
